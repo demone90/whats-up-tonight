c30fd1b294ce79da6b847825009f02d97b00b79a
Giuseppe Aina <ga@plista.com>
2017-07-07 17:29:40 +0200

    Play with jade and stylus adding scrollable clubs page #playground

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express'),
    logger = require('morgan'),
    app = express(),
-   template = require('jade').compileFile(__dirname + '/source/templates/homepage.jade')
+   template = require('jade').compileFile(__dirname + '/source/templates/homepage.jade'),
+   clubs = require('jade').compileFile(__dirname + '/source/templates/clubs.jade')
 
 app.use(logger('dev'))
 app.use(express.static(__dirname + '/static'))
+
 
 app.get('/', function (req, res, next) {
   try {
@@ -13,6 +15,15 @@ app.get('/', function (req, res, next) {
   } catch (e) {
     next(e)
   }
+})
+
+app.get('/clubs', function (req, res, next) {
+	try {
+		var html = clubs({ title: 'Clubs'})
+		res.send(html)
+	} catch (e) {
+		next(e)
+	}
 })
 
 app.listen(process.env.PORT || 3000, function () {
